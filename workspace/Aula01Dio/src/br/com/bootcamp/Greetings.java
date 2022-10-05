056b57a6c8be3a937e27962fdcf02abc2f335ff3
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-10-04 21:24:28 -0300

    Greetings Exercise

--- workspace/Aula01Dio/src/br/com/bootcamp/Greetings.java
+++ workspace/Aula01Dio/src/br/com/bootcamp/Greetings.java
@@ -1,2 +1,23 @@
-package br.com.bootcamp;public class Greetings {
+package br.com.bootcamp;
+
+public class Greetings {
+    public static String getMessage(double hour) {
+      if (hour >= 5 && hour <= 12) {
+        return "Good Morning";
+      }
+
+      else if (hour >= 13 && hour <= 17) {
+        return "Good Afternoon";
+      }
+      
+      else if (hour >= 18 && hour <= 23) {
+        return "Good Evining";
+      }
+      
+      else if (hour >= 0  && hour <= 4) {
+        return "Good Night";
+      }
+      return "Wrong number time";
+
+    }
 }
--- workspace/Aula01Dio/src/br/com/bootcamp/Main.java
+++ workspace/Aula01Dio/src/br/com/bootcamp/Main.java
@@ -1,17 +1,20 @@
 package br.com.bootcamp;
 
 public class Main {
-    public static void main(String[] args) {
-        double soma = new Calculadora().sum(2,3);
-        System.out.println(soma);
+  public static void main(String[] args) {
+    System.out.println(Calculadora.sum(2, 3));
 
-        double subtration = new Calculadora().subtration(2,3);
-        System.out.println(subtration);
+         System.out.println(Calculadora.subtration(3, 2));
 
-        double division = new Calculadora().division(2,3);
-        System.out.println(division);
+         System.out.println(Calculadora.division(3, 2));
 
-        double multiplication= new Calculadora().multiplication(2,3);
-        System.out.println(multiplication);
+         System.out.println(Calculadora.multiplication(3, 2));
+
+         System.out.println(Greetings.getMessage(6));
+
+         System.out.println(Greetings.getMessage(14));
+         System.out.println(Greetings.getMessage(21));
+         System.out.println(Greetings.getMessage(3));
+        
     }
 }
