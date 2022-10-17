21c95aa5c7c06d1c21d88ec23c6f51f5f6afa22c
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-10-17 06:31:38 -0300

    Create get and sets, other method

--- workspace/Collections/src/poo/Carro.java
+++ workspace/Collections/src/poo/Carro.java
@@ -5,5 +5,37 @@ public class Carro {
   String collor;
   int fuelCapacity;
 
+  public Carro(String name, String collor, int fuelCapacity) {
+    this.name = name;
+    this.collor = collor;
+    this.fuelCapacity = fuelCapacity;
+  }
 
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCollor() {
+    return this.collor;
+  }
+
+  public void setCollor(String collor) {
+    this.collor = collor;
+  }
+
+  public int getFuelCapacity() {
+    return this.fuelCapacity;
+  }
+
+  public void setFuelCapacity(int fuelCapacity) {
+    this.fuelCapacity = fuelCapacity;
+  }
+
+  double totalFuelCapacity(double fuelValue) {
+    return this.fuelCapacity * fuelValue;
+  }
 }
