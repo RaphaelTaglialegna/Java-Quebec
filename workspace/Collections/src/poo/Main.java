3dd6cf7c1e851a089b6f2d9a5da4d7d7d8fcfbb3
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-10-17 06:44:42 -0300

    Create main class and intance a new object

---
+++ workspace/Collections/src/poo/Main.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class Main {
+  public static void main(String[] args) {
+    Carro carro1 = new Carro("Gol", "Preto", 56);
+    
+    System.out.println(carro1.getName());
+    System.out.println(carro1.getCollor());
+    System.out.println(carro1.getFuelCapacity());
+    System.out.println(carro1.totalFuelCapacity(2.50));
+  }
+}
