91a1efbec1f66117e6c12bec29f59cebd0207b5b
RaphaelTaglialegna <raphael.trcosta@gmail.com>
2022-11-09 05:35:33 -0300

    Spring project

--- workspace/TestingJavaMaven/src/test/java/br/pessoa/PessoaTest.java
+++ workspace/TestingJavaMaven/src/test/java/br/pessoa/PessoaTest.java
@@ -12,4 +12,21 @@ public class PessoaTest {
         Pessoa pessoa = new Pessoa("Julia", LocalDateTime.of(2020, 1, 1, 15, 0, 0));
         Assertions.assertEquals(2, pessoa.getIdade());
     }
+
+    @Test
+    void  deveCalcularIdadeCorretamente() {
+        Pessoa jessica = new Pessoa("Jessica",LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+        Assertions.assertEquals(22, jessica.getIdade());
+    }
+    @Test
+    void  deveRetornarMaiorIdade() {
+        Pessoa jessica = new Pessoa("Jessica",LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+        Assertions.assertTrue(jessica.ehMaiorDeIdade());
+
+        Pessoa julia = new Pessoa("Julia", LocalDateTime.of(2020, 1, 1, 15,0,0));
+        Assertions.assertFalse(julia.ehMaiorDeIdade());
+
+        
+    }
+
 }
---
+++ workspace/springProject/src/test/java/br/com/firstSpringProject/springProject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.firstSpringProject.springProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
